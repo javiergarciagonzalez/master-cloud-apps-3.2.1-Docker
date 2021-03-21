fd4ab3eefecd635c6c42ebca58bc9f168eb51bd1
Javier García González <javier@garciagon.com>
2021-03-21 14:51:42 +0100

    Dockerize server

--- server/src/connections/mysqlConnection.js
+++ server/src/connections/mysqlConnection.js
@@ -4,7 +4,16 @@ import DebugLib from 'debug';
 
 const debug = new DebugLib('server:mysql');
 
-export default new Sequelize('eoloplantsDB', 'root', 'password', {
+console.log('process.env.toString()');
+console.log(process.env.DATABASE_HOST);
+
+const dbHost = process.env.DATABASE_HOST || 'eoloplantsDB';
+const dbUser = process.env.DATABASE_USER || 'root';
+const dbPass = process.env.DATABASE_PASS || 'password';
+
+export default new Sequelize(dbHost, dbUser, dbPass, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql',
     dialectModule: mysql2,
     logging: false
